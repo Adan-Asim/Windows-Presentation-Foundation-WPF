4041e3b5e3c5923afd151a59e2e7c9be8e751e7c
Adan Asim <67644268+Adan-Asim@users.noreply.github.com>
2021-07-26 15:16:56 +0500

    Add files via upload

---
+++ Restaurant Point Of Sale System POS/Assignment 5/Database/DB.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Data.SqlClient;
+using Assignment_5;
+
+namespace Assignment_5.Database
+{
+    class DB
+    {
+        static SqlConnection con= new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\ADAN ASIM\Desktop\ITC Assignment\EAD\Assignments\Assignment 5\Assignment 5\Database\Database.mdf;Integrated Security=True");
+
+        public static bool Insert(double Amount, List<Order> OL)
+        {
+            try
+            {
+                con.Open();
+                string query1 = $"DECLARE @OutputTbl TABLE(ID INT) INSERT INTO Orders(Amount, Date) OUTPUT INSERTED.OrderNo INTO @OutputTbl(ID) VALUES({ Amount}, '{DateTime.Now}') ";
+
+                foreach (Order o in OL)
+                {
+                    string part = $"INSERT INTO Items(Item, OrderNo) VALUES ('{o.title}', (Select ID from @OutputTbl))";
+                    query1 += part;
+                }
+
+                SqlCommand cmd = new SqlCommand(query1, con);
+                int rows = cmd.ExecuteNonQuery();
+
+                con.Close();
+                if (rows > 0)
+                    return true;
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+            return false;
+        }
+    }
+}
